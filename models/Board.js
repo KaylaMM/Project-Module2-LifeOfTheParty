c9c6dbf2751eb4c11a11c536d9428d49be0dbde2
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-02-15 17:36:28 -0500

    Repo clean

--- models/Board.js
+++ models/Board.js
@@ -8,35 +8,36 @@ const { Schema, model } = mongoose;
 // notice that when you reference you use the capitalization of the model that it is referencing
 
 const boardSchema = new Schema(
-    {
-        // the user that created the board
-        author: {
-            type: Schema.Types.ObjectId
-        },
-        // the title for the board that will be displayed to users
-        title: {
-            type: String
-        },
-        // the messages that belong to this board
-        messages: {
-            type: [
-                {
-                    type: Schema.Types.ObjectId,
-                    ref: "Message"
-                }
-            ]
-        },
-        // the users that are following this board
-        followers: {
-            type: [
-                {
-                    type: Schema.Types.ObjectId,
-                    ref: "User"
-                }
-            ]
-        }
+  {
+    // the user that created the board
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
     },
-    { timestamps: true }
+    // the title for the board that will be displayed to users
+    title: {
+      type: String
+    },
+    // the messages that belong to this board
+    messages: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Message"
+        }
+      ]
+    },
+    // the users that are following this board
+    followers: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User"
+        }
+      ]
+    }
+  },
+  { timestamps: true }
 );
 
 const Board = model("Board", boardSchema);
