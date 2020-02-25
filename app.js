70484704fbd58b472b01b5c6aad10c062673b9b2
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-02-24 21:33:52 -0500

    CRUD done, mic drop

--- app.js
+++ app.js
@@ -47,13 +47,13 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 
 // Express View engine setup
-app.use(
-    require("node-sass-middleware")({
-        src: path.join(__dirname, "public"),
-        dest: path.join(__dirname, "public"),
-        sourceMap: true
-    })
-);
+// app.use(
+//     require("node-sass-middleware")({
+//         src: path.join(__dirname, "public"),
+//         dest: path.join(__dirname, "public"),
+//         sourceMap: true
+//     })
+// );
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
