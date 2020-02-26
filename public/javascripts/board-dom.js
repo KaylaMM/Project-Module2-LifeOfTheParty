78cb2de6feffb00bc9750a190a60ea24fb15b14f
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-02-25 21:58:10 -0500

    Adding user CRUD rules, finishing board CRUD

--- public/javascripts/board-dom.js
+++ public/javascripts/board-dom.js
@@ -1,13 +1,18 @@
-document.querySelector('#update-board-button').onclick = () => {
-    const boardTitle = document.getElementById(boardTitle.value)
+window.onload = () => {
+  document.querySelector('#update-board-button').onclick = () => {
+    const boardTitle = document.getElementById("boardTitle").value;
+    const boardId = document.getElementById("boardId").value;
     document.getElementById('board-details').innerHTML = `
-    <form class="auth-form" action="/users/profile/update" method="POST" id="form-container" enctype="multipart/form-data">
-      <br><br>
-      <li> Board Name: <input id="title" type="text" name="title" value=${boardTitle}>
-      <br><br>
-      <button type="submit">Update details</button>
-      <form class="auth-form" action="/delete/:boardId" method="POST" id="form-container">
-        <button type="submit">Delete profile</button>
-      </form>
-    </form>`
-  }
+      <form class="auth-form" action="/boards/update/${boardId}" method="POST" id="form-container">
+        <br><br>
+        <ul>
+          <li>Board Name: <input id="title" type="text" name="title" value=${boardTitle}>
+        </ul>
+        <br><br>
+        <button type="submit">Update board</button>
+        </form>
+        <form class="auth-form" action="/boards/delete/${boardId}" method="POST" id="form-container">
+          <button type="submit">Delete board</button>
+        </form>`
+    };
+}
--- routes/boards/boards.js
+++ routes/boards/boards.js
@@ -4,79 +4,22 @@ const Board = require("../../models/Board");
 const User = require("../../models/User");
 const Memes = require('../../models/Memes');
 
-// Display Profile
-router.get("/", (req, res, next) => {
-  Board.find()
-    .then(allBoards => {
-      const boardsArray = allBoards.map(board => {
-        const obj = {
-          ...board._doc,
-          isOwner: req.session.user
-            ? board.author._id.toString() === req.session.user._id.toString()
-            : false
-        };
-        // board.isOwner = board.author._id === req.session.user._id;
-        return obj;
-      });
-
-      const data = {
-        boards: boardsArray,
-        // here we can use noBoards as a variable that can be called on the view page in order to use {{#if noBoards}} for conditional display.
-        // now we will be able to display one message if there are no boards (something like "There are no boards to display at this time. Be the first to create a new board!") or show the boards that exists if there are some
-        noBoards: boardsArray.length === 0
-      };
-      res.render("users/userProfile", data);
-    })
-    .catch(err => next(err));
-});
-
 //Route for board details
 router.get("/:boardId", (req, res, next) => {
-  console.log("here")
-  console.log(req.params.boardId);
   Board.findById(req.params.boardId)
     .populate("memes")
     .then(boardFromDB => {
-      console.log(boardFromDB);
-      res.render("boards/boardDetails", { boardFromDB });
+      res.render("boards/boardDetails", {boardFromDB});
     })
     .catch(err => console.log(err));
 });
 
-// display the specific board page
-router.get("/details/:boardId", (req, res, next) => {
-  // the findById below is using a deep populate which is used when you are going to populate the information within data that you are populating. Notice that unlike a normal populate, on a deep populate you have to set the path to the first data point that your going to populate and then path again into the data type that you just populated to find the other data.
-  // if there is more than one data point that you have to populate you can just link populates like the example below
-  // Board.findById(req.params.id).populate('messages').populate('followers')
-  Board.findById(req.params.boardId)
-    .populate({
-      path: "messages",
-      populate: [{ path: "replies" }, { path: "author" }]
-    })
-    .populate("followers")
-    .then(boardFromDB => {
-      const data = {
-        // this is a ternary operator. It is the same as doing an if else conditional statement but looks cleaner and can be used for conditional setting of values like the example below.
-        noMessages: boardFromDB.messages.length === 0 ? true : false,
-        board: boardFromDB,
-        messages: boardFromDB.messages.reverse()
-      };
-      // here we will set the local variable for the bodyClass to be something other than what we set in app.js in order to track the page change. We can then use this for styling or script file(which is what we will be using it for).
-      res.locals.bodyClass = "messageBoardDetails";
-
-      res.render("boards/boardDetails", data);
-    })
-    .catch(err => next(err));
-});
-
-// create a board for messages
+// Create a board for memes
 router.post("/create", (req, res, next) => {
   console.log("new board info >>>>>>>>>>>> ", req.body);
   console.log("req ==> ==> ==> ==> ==> ", req);
-  // for some of our routes, we may want the user to be logged in like on this one that a user is required in order to add the author to the board. For cases like this we can do a simple check for req.session.user and the user is not logged in then we can redirect them to the login page
   if (!req.session.user) {
     res.redirect("/auth/login");
-    // here we will add a return to stop the rest of the route from running otherwise we may get errors for not finding req.session.user or possibly create blank boards.
     return;
   }
 
@@ -103,27 +46,17 @@ router.post("/create", (req, res, next) => {
     .catch(err => next(err));
 });
 
-
+// Update board
 router.post("/update/:boardId", (req, res, next) => {
-  console.log(req.params.boardId)
-  Board.findByIdAndUpdate(req.params.boardId, {new: true})
-  .then( updatedBoard => {
-    console.log(updatedBoard)
-    res.render("/boards/boardDetails", {updatedBoard})
+  console.log(req.body);
+  Board.findByIdAndUpdate(req.params.boardId, {title: req.body.title}, {new: true})
+  .then(boardFromDB => {
+    console.log(boardFromDB)
+    res.render("boards/boardDetails", {boardFromDB})
   }).catch(err => console.log(err));
 })
 
-
-
-
-
-
-
-
-
-
-
-// delete a board, only the author or admin should be able too delete a board
+// Delete board
 router.post("/delete/:boardId", (req, res, next) => {
   if (!req.session.user) {
     res.redirect("/auth/login");
@@ -133,11 +66,13 @@ router.post("/delete/:boardId", (req, res, next) => {
     .then(() => {
       User.findByIdAndUpdate(
         req.session.user._id,
-        { $pull: { userBoards: req.params.boardId } },
-        { new: true }
+        {$pull: {userBoards: req.params.boardId}},
+        {new: true}
       )
-        .then(() => {
-          res.redirect("/boards");
+        .then(updatedUser => {
+          req.session.user = updatedUser;
+          res.locals.currentUser = req.session.user;
+          res.redirect(`/users/profile`);
         })
         .catch(err => next(err));
     })
@@ -159,24 +94,6 @@ router.post("/followers/:boardId", (req, res, next) => {
           res.redirect("back");
         })
         .catch(err => next(err));
-    })
-    .catch(err => next(err));
-});
-
-
-
-router.get("/add-message/:boardId/:messageId", (req, res, next) => {
-  // when updating we must add {new: true} in order to get the updated information from the db, otherwise you will get the information that is on the db prior to the update
-  Board.findByIdAndUpdate(
-    req.params.boardId,
-    {
-      $push: { messages: req.params.messageId }
-    },
-    { new: true }
-  )
-    .then(updatedBoard => {
-      res.status(200).json(updatedBoard);
-      // next();
     })
     .catch(err => next(err));
 });
